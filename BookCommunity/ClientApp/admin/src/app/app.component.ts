651a82a7ec3a9264dfa45738a0484e1a03af47f6
Han Van Hiep <quanghiepth86@gmail.com>
2017-11-21 23:27:36 +0700

    Remove not necessary code

--- BookCommunity/ClientApp/admin/src/app/app.component.ts
+++ BookCommunity/ClientApp/admin/src/app/app.component.ts
@@ -1,34 +1,9 @@
 import { Component } from "@angular/core";
-import { Router } from "@angular/router";
-
-import { AuthService } from "./services/auth.service";
-import { ConfimService } from "./services/confirm.service";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"],
-  providers: [ConfimService]
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-    public isLogged = false;
-
-    constructor(
-        private router: Router,
-        private authService: AuthService,
-        private confirmService: ConfimService
-    ) {
-        this.authService.getLoginStatus().subscribe(result => {
-            this.isLogged = result;
-            if (!this.isLogged) {
-                this.router.navigate(["/cpanel/login"]);
-            }
-        });
-
-        this.confirmService.confirmLogin$.subscribe(result => {
-            console.log("result", result);
-            this.isLogged = result;
-            this.router.navigate(["/admin"]);
-        });
-    }
 }
